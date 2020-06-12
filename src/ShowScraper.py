d3a894ce5c47f341ffe6f4ba585fb1f8a0f74171
Indra Febrio Nugroho <indrafbrngrh@gmail.com>
2020-06-12 14:49:02 +0700

    deleted SingleShowScraper.py and added 80 scraped data

--- src/ShowScraper.py
+++ src/ShowScraper.py
@@ -5,37 +5,44 @@ import requests
 import json
 from time import sleep
 
-url = 'https://www.themoviedb.org/tv/top-rated'
-response = requests.get(url, timeout=5)
-content = BeautifulSoup(response.content, "html.parser")
+with open('../data/topRatedTVShowData.json', 'r') as infile:
+    showArr = json.load(infile)
 
-showArr = []
-for show in content.findAll('div', attrs={"class": "card style_1"}):
-    show_url = show.find('a', attrs={"class": "image"})['href'].strip()
-    show_response = requests.get('https://www.themoviedb.org' + show_url, timeout=5)
-    show_content = BeautifulSoup(show_response.content, "html.parser")
+header = {'user-agent': 'Indra Nugroho/indrafbrngrh@gmail.com'}
 
-    single_show = show_content.find('div', attrs={"class": "header large border first"})
-    genre = single_show.find('span', attrs={"class": "genres"})
-    synopsis = single_show.find('div', attrs={"class": "overview"})
-    rating = show.find('div', attrs={"class": "outer_ring"})
-    certification = single_show.find('span', attrs={"class": "certification"})
-    if (certification is not None):
-    	certification = certification.text.strip()
+url = 'https://www.themoviedb.org/tv/top-rated?page='
 
-    showObject = {
-        "title": show.find('a', attrs={"class": "image"})['title'].strip(),
-        "synopsis": synopsis.find('p').text.strip(),
-        "genre": [g.text.strip() for g in genre.findAll('a')],
-        "rating": rating.find('div')['data-percent'].strip(),
-        "release_date": show.find('p').text.strip(),
-        "duration": single_show.find('span', attrs={"class": "runtime"}).text.strip(),
-        "certification": certification
-    }
+for i in range(2,6):
+    print(i)
+    response = requests.get(url + str(i), headers=header, timeout=5)
+    content = BeautifulSoup(response.content, "html.parser")
 
-    showArr.append(showObject)
+    for show in content.findAll('div', attrs={"class": "card style_1"}):
+        show_url = show.find('a', attrs={"class": "image"})['href'].strip()
+        show_response = requests.get('https://www.themoviedb.org' + show_url, timeout=5)
+        show_content = BeautifulSoup(show_response.content, "html.parser")
 
-    sleep(5)
+        single_show = show_content.find('div', attrs={"class": "header large border first"})
+        genre = single_show.find('span', attrs={"class": "genres"})
+        synopsis = single_show.find('div', attrs={"class": "overview"})
+        rating = show.find('div', attrs={"class": "outer_ring"})
+        certification = single_show.find('span', attrs={"class": "certification"})
+        if (certification is not None):
+           certification = certification.text.strip()
+
+        showObject = {
+            "title": show.find('a', attrs={"class": "image"})['title'].strip(),
+            "synopsis": synopsis.find('p').text.strip(),
+            "genre": [g.text.strip() for g in genre.findAll('a')],
+            "rating": rating.find('div')['data-percent'].strip(),
+            "release_date": show.find('p').text.strip(),
+            "duration": single_show.find('span', attrs={"class": "runtime"}).text.strip(),
+            "certification": certification
+        }
+
+        showArr.append(showObject)
+
+        sleep(10)
 
 with open('../data/topRatedTVShowData.json', 'w') as outfile:
     json.dump(showArr, outfile, indent=4)
