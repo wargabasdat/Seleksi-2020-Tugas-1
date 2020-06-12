145d3f65192f2bba63846e6cafaf7796bf27fd20
Indra Febrio Nugroho <indrafbrngrh@gmail.com>
2020-06-12 10:33:01 +0700

    added json data attributes

--- src/ShowScraper.py
+++ src/ShowScraper.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-
+from time import sleep
 
 url = 'https://www.themoviedb.org/tv/top-rated'
 response = requests.get(url, timeout=5)
@@ -11,15 +11,31 @@ content = BeautifulSoup(response.content, "html.parser")
 
 showArr = []
 for show in content.findAll('div', attrs={"class": "card style_1"}):
-    rating_ring = show.find('div', attrs={"class": "outer_ring"})
+    show_url = show.find('a', attrs={"class": "image"})['href'].strip()
+    show_response = requests.get('https://www.themoviedb.org' + show_url, timeout=5)
+    show_content = BeautifulSoup(show_response.content, "html.parser")
+
+    single_show = show_content.find('div', attrs={"class": "header large border first"})
+    genre = single_show.find('span', attrs={"class": "genres"})
+    synopsis = single_show.find('div', attrs={"class": "overview"})
+    rating = show.find('div', attrs={"class": "outer_ring"})
+    certification = single_show.find('span', attrs={"class": "certification"})
+    if (certification is not None):
+    	certification = certification.text.strip()
+
     showObject = {
-        "title": show.find('a', attrs={"class": "image"})['title'].encode('utf-8').decode(),
-        "site": show.find('a', attrs={"class": "image"})['href'].encode('utf-8').decode(),
-        "picture": show.find('img', attrs={"class": "poster lazyload lazyloaded"})['alt src'].encode('utf-8').decode(),
-        "rating": rating_ring.find('div')['data-percent'].encode('utf-8').decode(),
-        "release_date": show.find('p').text.encode('utf-8').decode()
+        "title": show.find('a', attrs={"class": "image"})['title'].strip(),
+        "synopsis": synopsis.find('p').text.strip(),
+        "genre": [g.text.strip() for g in genre.findAll('a')],
+        "rating": rating.find('div')['data-percent'].strip(),
+        "release_date": show.find('p').text.strip(),
+        "duration": single_show.find('span', attrs={"class": "runtime"}).text.strip(),
+        "certification": certification
     }
+
     showArr.append(showObject)
 
+    sleep(5)
+
 with open('../data/topRatedTVShowData.json', 'w') as outfile:
-    json.dump(showArr, outfile)
+    json.dump(showArr, outfile, indent=4)
--- src/SingleShowScraper.py
+++ src/SingleShowScraper.py
@@ -1,5 +1,9 @@
 # SingleShowScraper.py
 
+from bs4 import BeautifulSoup
+import requests
+import json
+
 url = 'https://www.themoviedb.org/tv/100-i-am-not-an-animal'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
@@ -8,9 +12,9 @@ show = content.find('section', attrs={"class": "header poster no_backdrop"})
 genre = show.find('span', attrs={"class": "genres"})
 synopsis = show.find('div', attrs={"class": "overview"})
 showObject = {
-    "certification": show.find('span', attrs={"class": "certification"}).text.strip().encode('utf-8').decode(),
-    "genre": [g.text.encode('utf-8').decode() for g in genre.findAll('a')],
-    "duration": show.find('span', attrs={"class": "runtime"}).text.strip().encode('utf-8').decode(),
-    "synopsis": synopsis.find('p').text.encode('utf-8').decode(),
+    "certification": show.find('span', attrs={"class": "certification"}).text.strip(),
+    "genre": [g.text.strip() for g in genre.findAll('a')],
+    "duration": show.find('span', attrs={"class": "runtime"}).text.strip(),
+    "synopsis": synopsis.find('p').text.strip(),
 }
 print(showObject)
