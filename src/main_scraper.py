f7df0b3874ebefd55f433794e7ad67da4040f42b
regnents <rafaelseanputra@gmail.com>
2020-06-14 19:39:33 +0700

    edit main_scraper.py

--- src/main_scraper.py
+++ src/main_scraper.py
@@ -5,32 +5,30 @@ from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor, Executor, wait
 import time
 import threading
+import json
 
 gembok_error = threading.Lock()
 gembok_cetak = threading.Lock()
 gembok_link = threading.Lock()
 gembok_penambah = threading.Lock()
+gembok_data = threading.Lock()
 
 executor = ThreadPoolExecutor(50)
 pengakses = 0
 bulan = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-# error_count = 0
-# daftar_error = []
-conn = None
-curs = None
+data = []
+done = 0
+errored = 0
 
 # User scraper1
 # pass scrapingnow
 
 
 def soup_creator(url):
-    # Fungsi soup_creator
-    # Digunakan untuk menghasilkan soup dari link yang diberikan
-    # VAR url merupakan string berupa link yang akan dibuka dan dibuat soup
     global pengakses
-    # global error_count
-    # global daftar_error
+    
+    global errored
     while pengakses > 20:
         gembok_link.acquire()
         time.sleep(5)
@@ -41,12 +39,13 @@ def soup_creator(url):
                   'user-agent': 'Windows NT 10.0; Win64; x64; Rafael Sean Putra/13518119@std.stei.itb.ac.id'})
     try:
         url_opened = urlopen(req)
-        # print("done link")
     except:
         gembok_error.acquire()
-        print("error accessing %s", url)
+        print("error accessing ", url)
+        errored += 1
         gembok_error.release()
-        raise Exception("error mengakses link yang diberikan")
+        msg = "error mengakses " + url
+        raise Exception(msg)
     url_read = url_opened.read()
     soup = BeautifulSoup(url_read, 'html.parser')
     url_opened.close()
@@ -59,27 +58,20 @@ def soup_creator(url):
 
 
 def data_scraper(url):
-    # Prosedure data_scraper
-    # VAR url sebagai link menuju ke laman kecelakaan yang akan diambil informasinya
-    # Inisialisasi data yang dibutuhkan
-    date = ""
-    jam = ""
-    operator = ""
-    crew = -999
-    pax = -999
-    crew_death = -999
-    pax_death = -999
-    other_death = -999
-
+    global done
+    global data
     # Membuat BeautifulSoup dari link artikel
     try:
         article_soup = soup_creator(url)
-    except:
-        # gembok_error.acquire()
-        # # print("error" + url)
-        # gembok_error.release()
-        return
-    # Mencari tanggal kejadian
+        gembok_cetak.acquire()
+        done += 1
+        gembok_cetak.release()
+    except Exception as err:
+        raise Exception(err.format())
+        return;
+
+    # Tanggal kejadian
+    date = ''
     article_date = article_soup.find("div", {'class': 'crash-date'}).text
     test = article_date[34:37]
     idx_bulan = 0
@@ -105,56 +97,74 @@ def data_scraper(url):
         date += tgl
     date = "-" + date
     date = article_date[i_date+2:i_date+6] + date
+    # print(date)
 
-    # Mencari waktu kejadian
+    # Waktu kejadian
+    jam = ''
     if (article_date[i_date+7] == "a"):
         jam = jam + article_date[i_date+10:i_date+12] + \
             ":" + article_date[i_date+12:i_date+14]
     else:
-        jam = 'NULL'
+        jam = 'Data tidak tersedia'
+    # print(jam)
 
-    # Mencari jenis pesawat yang mengalami kejadian
-    airplane = article_soup.find(
-        "div", {'class': 'crash-aircraft'}).find("div").text
-
-    # Mencari maskapai/operator penerbangan dari pesawat yang mengalami kejadian
-    article_operator = article_soup.find('div', {'class': 'crash-operator'})
-    if (article_operator.find('img')):
-        img_operator = article_operator.find('img')['src']
-        i_operator = len(img_operator) - 5
-        while (img_operator[i_operator] != '/'):
-            i_operator -= 1
-        operator = img_operator[i_operator+1:len(img_operator)-4]
-        operator = operator.replace("%20", " ")
-        if (operator[len(operator)-2] == "-"):
-            operator = operator[:len(operator)-2]
+    # Jenis Pesawat dalam Kejadian
+    airplane = ''
+    if (article_soup.find('div',{'class':'crash-aircraft'})):
+        airplane += article_soup.find("div", {'class': 'crash-aircraft'}).find("div").text
     else:
-        operator = article_operator.find('div').text
+        airplane = 'Data tidak tersedia'
+    # print(airplane)
 
-    # Mencari negara
-    country = article_soup.find(
-        'div', {'class': 'crash-country'}).find('div').text
-    # print('done country')
+    # Maskapai/Operator penerbangan pada Kejadian
+    operator = ''
+    if (article_soup.find('div',{'class':'crash-operator'})):
+        article_operator = article_soup.find('div', {'class': 'crash-operator'})
+        if (article_operator.find('img')):
+            img_operator = article_operator.find('img')['src']
+            i_operator = len(img_operator) - 5
+            while (img_operator[i_operator] != '/'):
+                i_operator -= 1
+            operator = img_operator[i_operator+1:len(img_operator)-4]
+            operator = operator.replace("%20", " ")
+            if (operator[len(operator)-2] == "-"):
+                operator = operator[:len(operator)-2]
+        else:
+            operator += article_operator.find('div').text
+    else:
+        operator = 'Data tidak tersedia'
+    # print(operator)
 
-    # Mencari jumlah kru, penumpang, dan korban meninggal
+    # Negara
+    country = ''
+    if (article_soup.find('div',{'class':'crash-country'})):
+        country += article_soup.find(
+            'div', {'class': 'crash-country'}).find('div').text
+    else:
+        country = 'Data tidak tersedia'
+    # print(country)
+
+    # Jumlah kru, penumpang, dan korban meninggal dunia
     if (article_soup.find('div', {'class': 'crash-crew-on-board'})):
         crew = int(article_soup.find(
             'div', {'class': 'crash-crew-on-board'}).find('div').text)
+    else:
+        crew = 'Data tidak tersedia'
     crew_death = int(article_soup.find(
         'div', {'class': 'crash-crew-fatalities'}).find('div').text)
     if (article_soup.find('div', {'class': 'crash-pax-on-board'})):
         pax = int(article_soup.find(
             'div', {'class': 'crash-pax-on-board'}).find('div').text)
+    else:
+        pax = 'Data tidak tersedia'
     pax_death = int(article_soup.find(
         'div', {'class': 'crash-pax-fatalities'}).find('div').text)
     other_death = int(article_soup.find(
         'div', {'class': 'crash-other-fatalities'}).find('div').text)
-    # print('done int')
-
-    # Testing, mencetak data
-    gembok_cetak.acquire()
-    gembok_cetak.release()
-
+    
+    gembok_data.acquire()
+    data.append({'Accident Date':date,'Accident Time':jam,'Airplane Operator' :operator,'Airplane Type':airplane,'Crash Location':country,'Crew':crew,'Crew Casualties':crew_death,'Passenger':pax,'Passenger Casualties':pax_death,'Other Casualties':other_death})
+    gembok_data.release()
 
 def search_page_iterative(search_url, i):
     # Prosedur search_page_iterative
@@ -164,9 +174,9 @@ def search_page_iterative(search_url, i):
     url = search_url + str(i)
     try:
         test_soup = soup_creator(url)
-    except:
-        # print("error accesing %s",url)
-        return
+    except Exception as err:
+        print(err.format())
+        return;
     links_container = test_soup.findAll("a", {"class": "red-btn"})
     for link in links_container:
         output = "https://www.baaa-acro.com" + link['href']
@@ -175,16 +185,12 @@ def search_page_iterative(search_url, i):
 
 if __name__ == "__main__":
     start_url = "https://www.baaa-acro.com/crash-archives?field_crash_flight_type_target_id=12996&page="
+    t_start = time.perf_counter()
     for i in range(0, 62):
         search_page_iterative(start_url, i)
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/crash-boeing-737-524-usinsk")
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/crash-piper-pa-31t-cheyenne-near-eatonton-5-killed")
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/crash-farman-f60-goliath-belgium")
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/ground-fire-airbus-a330-343-beijing")
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/crash-britten-norman-bn-2b-27-islander-puerto-montt-6-killed")
-    # executor.submit(data_scraper, "https://www.baaa-acro.com/crash/crash-cessna-208b-grand-caravan-near-tastiota-2-killed")
+    # executor.submit(data_scraper,'https://www.baaa-acro.com/crash/crash-airbus-a320-214-karachi-97-killed')
     executor.shutdown(wait=True)
-    # print("Total data error= " + str(error_count))
-    # if error_count > 0:
-    #     for error in daftar_error:
-    #         print("error link: %s", error)
+    t_stop = time.perf_counter()
+    print(t_stop - t_start)
+    with open('data/data.json','w') as f:
+        json.dump(data,f,indent=4,ensure_ascii=False)
